d116a0001e54c1ece6f576ec64fad8601548c5ac
Yusuff Jamal <yusuffjamal3@gmail.com>
2024-09-27 01:25:35 +0200

    Some Progress

--- chromosome.py
+++ chromosome.py
@@ -1,5 +1,4 @@
 import math
-import json
 import random
 import numpy as np
 
@@ -38,16 +37,12 @@ def find_equidistant_points(points, num_points):
     return equidistant_points
 
 
-def is_not_in_circle_range(p1, p2, radius):
-    return distance(p1, p2) > radius
-
-
 class Magnet:
     def __init__(self, x, y, duration):
         self.x = x
         self.y = y
         self.active = False
-        self.duration = 10
+        self.duration = duration
         self.strength = 50  # newtons
 
     def activate(self):
@@ -55,9 +50,6 @@ class Magnet:
 
     def to_tuple(self):
         return (self.x, self.y)
-
-    def update(self, dt):
-        self.duration -= dt
 
     def deactivate(self):
         self.active = False
@@ -67,67 +59,104 @@ class Magnet:
 
 
 class Chromosome:
-    def __init__(self, num_magnets, mass, dt, target_path) -> None:
+    def __init__(self, num_magnets, mass, time_step, target_path):
         self.num_magnets = num_magnets
         self.mass = mass  # kg
-        self.dt = dt  # seconds
-        self.proximity_radius = random.uniform(0.01, 0.1)
+        self.time_step = time_step  # seconds
+        self.actual_times = []
         self.initialize_magnets(target_path)
 
     def to_dict(self):
         return {
             "num_magnets": self.num_magnets,
-            "proximity_radius": self.proximity_radius,
             "magnets": [magnet.to_dict() for magnet in self.magnets],
         }
 
     def initialize_magnets(self, target_path):
         positions = find_equidistant_points(target_path, self.num_magnets)
-        self.magnets = [Magnet(*pos, random.uniform(0.1, 5.0)) for pos in positions]
+        self.magnets = [Magnet(*pos, random.uniform(0.1, 2.0)) for pos in positions]
+
+    def estimate_time_to_magnet(self, ball_pos, velocity, magnet_pos, acceleration):
+        displacement = magnet_pos - ball_pos
+
+        # Quadratic equation coefficients
+        a = 0.5 * np.dot(acceleration, acceleration)
+        b = np.dot(velocity, acceleration)
+        c = np.dot(displacement, displacement)
+
+        # Solve quadratic equation
+        discriminant = b**2 - 4 * a * c
+        if discriminant < 0:
+            # Ball won't reach the magnet with current parameters
+            return float("inf")
+
+        t1 = (-b + np.sqrt(discriminant)) / (2 * a)
+        t2 = (-b - np.sqrt(discriminant)) / (2 * a)
+
+        # Return the smallest positive solution
+        return min(t for t in (t1, t2) if t > 0) if (t1 > 0 or t2 > 0) else float("inf")
 
     def simulate_path(self, target_path):
-        start, dest = target_path[0], target_path[-1]
-
+        start = target_path[0]
         simulated_path = [start]
+
         velocity = np.array([0.0, 0.0])
+        ball_pos = np.array(start)
+        actual_times = []
 
         for magnet in self.magnets:
             magnet.activate()
-            ball_pos = np.array(simulated_path[-1])
-            magnet_pos = np.array(magnet.to_tuple())
+            time_elapsed = 0
 
-            while magnet.duration > 0:
+            while time_elapsed < magnet.duration:
+                magnet_pos = np.array(magnet.to_tuple())
                 direction = magnet_pos - ball_pos
                 dist = np.linalg.norm(direction)
 
-                if dist < self.proximity_radius:
-                    velocity_reduction_factor = max(
-                        0, (dist / self.proximity_radius) ** 2
-                    )
-                    velocity *= velocity_reduction_factor
+                if dist > 0:
+                    acceleration = direction / dist
+                else:
+                    acceleration = np.array([0.0, 0.0])
 
-                    if dist < self.proximity_radius * 0.1:
-                        velocity = np.array([0.0, 0.0])
-                        break
+                velocity += acceleration * self.time_step
+                predicted_pos = ball_pos + velocity * self.time_step
 
-                unit_direction = direction / dist
-                force_magnitude = magnet.strength / (dist**2)
-                force = force_magnitude * unit_direction
+                magnet_direction_current = magnet_pos - ball_pos
+                magnet_direction_future = magnet_pos - predicted_pos
 
-                acceleration = force / self.mass
-                velocity += acceleration * self.dt
+                if np.dot(magnet_direction_current, magnet_direction_future) <= 0:
+                    # Magnet is between, stop the ball at the magnet
+                    distance_to_magnet = np.linalg.norm(magnet_direction_current)
+                    time_to_magnet = distance_to_magnet / np.linalg.norm(velocity)
+                    time_elapsed += time_to_magnet
 
-                ball_pos += velocity * self.dt + 0.5 * acceleration * self.dt**2
-                simulated_path.append(ball_pos.copy())
+                    ball_pos = magnet_pos
+                    velocity = np.array([0.0, 0.0])
+                    simulated_path.append(ball_pos.copy())
 
-                if ball_pos[0] > dest[0] or ball_pos[1] < 0:
                     break
+                else:
+                    # Update ball position for the next step
+                    ball_pos = predicted_pos
+                    simulated_path.append(ball_pos.copy())
 
-                magnet.update(self.dt)
+                time_elapsed += self.time_step
 
+            actual_times.append(time_elapsed)
             magnet.deactivate()
 
-            if np.allclose(ball_pos, dest) or ball_pos[0] > dest[0]:
-                break
-
+        self.actual_times = actual_times
         return simulated_path
+
+    def mutate(self, mutation_rate, mutation_magnitude):
+        for i, magnet in enumerate(self.magnets):
+            if random.random() < mutation_rate:
+                actual_time = self.actual_times[i]
+                time_difference = actual_time - magnet.duration
+
+                adaptive_mutation = time_difference * random.uniform(
+                    mutation_magnitude * 0.1, mutation_magnitude * 0.5
+                )
+                magnet.duration = max(0.1, magnet.duration + adaptive_mutation)
+
+        return self
